8007154ce03a0e0b08cb5dcf48f0849b8f5b2c82
dipalimagare <kalpeshmahajan90@gmail.com>
2021-08-15 23:13:42 +0530

    Uploading File

--- TestMaximum.java
+++ TestMaximum.java
@@ -2,36 +2,22 @@ package com.bl;
 
 import java.util.Scanner;
 
-public class TestMaximum {
-	Scanner sc = new Scanner(System.in);
+public class TestMaximum<t extends Comparable<t>> {
 
-	public Integer getMaxNumber(Integer num1, Integer num2, Integer num3) {
-		Integer max = num1;
-		if (num2.compareTo(max) > 0)
-			max = num2;
-		else if (num3.compareTo(max) > 0)
-			max = num3;
-
-		return max;
-	}
-
-	public Double getMaxNumber(Double num1, Double num2, Double num3) {
-		Double max = num1;
-		if (num2.compareTo(max) > 0)
-			max = num2;
-		else if (num3.compareTo(max) > 0)
-			max = num3;
-
-		return max;
-	}
-
-	public String getMaxStr(String num1, String num2, String num3) {
-		String max = num1;
-		if (max.compareTo(num2) > max.length())
-			max = num2;
-		if (max.compareTo(num3) > max.length())
-			max = num3;
-		System.out.println("====>" + max);
+	public static <t extends Comparable<t>> t getMaximumNumber(t... num) {
+		t max = num[0];
+		String t = "";
+		if (t.getClass() != max.getClass()) {
+			for (t element : num) {
+				if (element.compareTo(max) > 0)
+					max = element;
+			}
+		} else {
+			for (t element : num) {
+				if (element.compareTo(max) > ((String) max).length())
+					max = element;
+			}
+		}
 		return max;
 	}
 }
